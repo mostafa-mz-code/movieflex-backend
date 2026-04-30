263b0cd0035b480be8e5cff19a2a02beaffd33b2
mostafa <mostafa.mz.code@gmail.com>
2026-04-30 20:49:36 +0430

    update move repository

--- repositories/movie.repository.ts
+++ repositories/movie.repository.ts
@@ -1,25 +1,30 @@
 import { prisma } from "../lib/db";
 
-type Movie = {
+export type Movie = {
   title: string;
   searchTerm: string;
   posterUrl: string;
   movieId: string;
+  count: number;
 };
 
 export class MovieRepository {
-  async getMovies() {
+  async getAllMovies() {
     return prisma.movie.findMany();
+  }
+
+  async getMovie(movieId: string) {
+    return prisma.movie.findUnique({ where: { movieId } });
   }
 
   async createMovie(movie: Movie) {
     return prisma.movie.create({ data: movie });
   }
 
-  async updateMovie(id: number, count: number) {
+  async updateMovie(id: number, data: Movie) {
     return prisma.movie.update({
       where: { id },
-      data: { count },
+      data: { ...data },
     });
   }
 }

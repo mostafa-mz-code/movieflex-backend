ad99cd537446897d8f995555c0d3a5ff5b7923b3
mostafa <mostafa.mz.code@gmail.com>
2026-05-01 13:18:45 +0430

    update index file

--- index.ts
+++ index.ts
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Movie API!");
+});
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {

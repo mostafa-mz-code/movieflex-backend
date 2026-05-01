4777073d2a3578746dfe49e2d314e73f7fe720b1
mostafa <mostafa.mz.code@gmail.com>
2026-05-01 11:35:35 +0430

    prepare for vercel-deployment

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import serverless from "serverless-http";
 import dotenv from "dotenv";
 import express from "express";
 import router from "./routes";
@@ -7,8 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+export const handler = serverless(app);
